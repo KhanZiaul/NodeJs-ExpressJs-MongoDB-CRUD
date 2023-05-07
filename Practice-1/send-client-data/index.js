6199f6548eee0bab2777d7f1ad14a3a4ff904806
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-07 11:55:19 +0600

    users data added

--- Practice-1/send-client-data/index.js
+++ Practice-1/send-client-data/index.js
@@ -6,10 +6,28 @@ const cors = require('cors')
 
 app.use(cors())
 
+const users = [
+    {
+        "name": "John Doe",
+        "email": "johndoe@example.com",
+        "id": 1
+    },
+    {
+        "name": "Jane Smith",
+        "email": "janesmith@example.com",
+        "id": 2
+    },
+    {
+        "name": "Bob Johnson",
+        "email": "bjohnson@example.com",
+        "id": 3
+    }
+]
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
