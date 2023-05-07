31e8798d5f1bf2259260d6ddefbcf24236a7c53d
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-07 12:20:39 +0600

    form added

--- Practice-1/send_client_data_to_server/src/App.jsx
+++ Practice-1/send_client_data_to_server/src/App.jsx
@@ -9,18 +9,35 @@ const App = () => {
       .then(res => res.json())
       .then(datas => setUsers(datas))
   }, [])
+
+  function formHandler(event){
+    event.preventDefault()
+    const Name = event.target.name.value ;
+    const Email = event.target.email.value ;
+
+    console.log(Name,Email)
+  }
+
   return (
     <div>
       <h2>Send Client Data To Server</h2>
-      {
-        users?.map((user) => {
-          return (
-            <div>
-              <p>{user.id}</p>
-            </div>
-          )
-        })
-      }
+      <form onSubmit={formHandler}>
+        <input type="text" placeholder='name' name='name' required/> <br /> <br />
+        <input type="text" placeholder='email' name='email' required /> <br /> <br />
+        <input type="submit" value='Submit' />
+      </form>
+      <div>
+        {
+          users?.map((user) => {
+            return (
+              <div key={user.id}>
+                <p>{user.id} . {user.name} - {user.email}</p>
+              </div>
+            )
+          })
+        }
+
+      </div>
     </div>
   );
 };
