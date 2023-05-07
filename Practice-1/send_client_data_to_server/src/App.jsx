dff98d3e7ccd5b2b9939a1e7bfbf51620a134510
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-07 11:59:57 +0600

    fetch users data done

--- Practice-1/send_client_data_to_server/src/App.jsx
+++ Practice-1/send_client_data_to_server/src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css'
 
 const App = () => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:2000/users')
+      .then(res => res.json())
+      .then(datas => setUsers(datas))
+  }, [])
   return (
     <div>
       <h2>Send Client Data To Server</h2>
+      {
+        users?.map((user) => {
+          return (
+            <div>
+              <p>{user.id}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 };
