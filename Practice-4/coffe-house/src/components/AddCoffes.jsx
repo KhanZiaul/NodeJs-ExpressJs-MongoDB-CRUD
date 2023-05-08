f43b8aadf257ad5271c699ad95bf31efd4afa695
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-09 03:25:20 +0600

    AddCoffe updataed

--- Practice-4/coffe-house/src/components/AddCoffes.jsx
+++ Practice-4/coffe-house/src/components/AddCoffes.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
-
 const AddCoffes = () => {
     return (
-        <div>
-            
+        <div className='bg-slate-200 p-8'>
+            <h2 className='text-5xl text-center my-6'>Add Coffes</h2>
+            <form className="grid grid-cols-2">
+
+                <div className="form-control mb-7">
+                        <label className="input-group">
+                            <span>Name</span>
+                            <input  type="text" placeholder="Enter coffee name" name="coffe" className="input input-bordered w-full" />
+                        </label>
+                </div>
+                
+                <div className="form-control">
+                        <label className="input-group">
+                            <span>Chef</span>
+                            <input  type="text" placeholder="Enter coffee chef" name="chef" className="input input-bordered w-full" />
+                        </label>
+                </div>
+                
+                <div className="form-control">
+                        <label className="input-group">
+                            <span>Supplier</span>
+                            <input  type="text" placeholder="Enter coffee supplier" name="supplier"  className="input input-bordered w-full" />
+                        </label>
+                </div>
+                
+                <div className="form-control">
+                        <label className="input-group">
+                            <span>Name</span>
+                            <input  type="text" placeholder="Enter coffee chef" name="chef-name"  className="input input-bordered w-full" />
+                        </label>
+                </div>
+                
+                <div className="form-control">
+                        <label className="input-group">
+                            <span>Name</span>
+                            <input  type="text" placeholder="Enter coffee chef" name="chef-name"  className="input input-bordered w-full" />
+                        </label>
+                </div>
+                
+                <div className="form-control">
+                        <label className="input-group">
+                            <span>Name</span>
+                            <input  type="text" placeholder="Enter coffee chef" name="chef-name"  className="input input-bordered w-full" />
+                        </label>
+                </div>
+
+            </form>
         </div>
     );
 };
--- Practice-4/coffe-house/src/components/UpdateCoffe.jsx
+++ Practice-4/coffe-house/src/components/UpdateCoffe.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const UpdateCoffe = () => {
     return (
         <div>
-            
+            <h2 className='text-5xl text-center my-6'>Update Coffes</h2>
+
         </div>
     );
 };
