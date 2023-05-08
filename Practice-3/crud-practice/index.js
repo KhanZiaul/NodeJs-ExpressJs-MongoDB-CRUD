f4fa978c739af861a1341305ea18dfffe7603a90
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-08 12:27:04 +0600

    index.js updated

--- Practice-3/crud-practice/index.js
+++ Practice-3/crud-practice/index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion} = require('mongodb');
 const express = require('express');
 const app = express()
 const cors = require('cors');
@@ -8,10 +9,38 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/',(req,res) => {
+
+const uri = "mongodb+srv://khanzia16phy102:77GaYuM0Zhq04jBU@cluster0.mf37tl1.mongodb.net/?retryWrites=true&w=majority";
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        await client.connect();
+
+        await client.db("admin").command({ ping: 1 });
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('yah you done it')
 })
 
-app.listen(port , () => {
+app.listen(port, () => {
     console.log(`Port is running in - ${port}`)
 })
