c2ad68b5047b374acf5d4de0b1215d912c58de0d
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-08 15:53:57 +0600

    formHandler added in UpdateUSer

--- Practice-3/name-of-your-project/src/component/UpdateUser.jsx
+++ Practice-3/name-of-your-project/src/component/UpdateUser.jsx
@@ -3,11 +3,26 @@ import { useLoaderData } from 'react-router-dom';
 
 const UpdateUser = () => {
     const user = useLoaderData()
+
+    function formHandler(event) {
+        event.preventDefault()
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const user = { name, email }
+        console.log(user)
+        event.target.reset()
+    }
     return (
         <div>
-           <p>{user?.name}</p> 
-           <p>{user?.email}</p> 
-            
+            <h2>Update User</h2>
+            <form onSubmit={formHandler}>
+                <input type="text" name='name' placeholder='name' defaultValue={user?.name} required />
+                <br /> <br />
+                <input type="email" name='email' placeholder='email' defaultValue={user?.email} required />
+                <br /> <br />
+                <input type="submit" value='update user' required />
+            </form>
+
         </div>
     );
 };
