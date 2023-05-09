cf91140868717e4b4c3c863cb09a6dc396bdda76
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-09 13:20:03 +0600

    read coffe data done

--- Practice-4/coffe-house-server/index.js
+++ Practice-4/coffe-house-server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 2000
 
@@ -27,6 +27,19 @@ async function run() {
     await client.connect();
 
     const coffeCollections = client.db("coffesDB").collection("coffes");
+
+    app.get('/coffes',async(req,res) => {
+      const cursor = coffeCollections.find()
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/coffes/:id',async(req,res) => {
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)};
+      const result = await coffeCollections.findOne(query);
+      res.send(result)
+    })
 
     app.post('/coffes', async(req, res) => {
       const coffe = req.body
